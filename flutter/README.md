c28ce0772ad54a75547e643052943918bfc250fa
panyao <pan3yao@gmail.com>
2021-03-30 12:05:12 +0800

    flutter web build

--- flutter/README.md
+++ flutter/README.md
@@ -1,1 +1,15 @@
-git clone -b stable https://github.com/flutter/flutter.git
+# 环境设置
+## flutter
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+## jdk
+
+
+# 编译
+
+## web
+flutter build web
+pushd ./build/web ; python3 -m http.server ; popd
+
+
+# docker
+sudo docker run --rm -it -v ${PWD}:/build --workdir /build dockerlinz/flutter:stable
--- flutter/env.sh
+++ flutter/env.sh
@@ -4,6 +4,11 @@ homePath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 workPath=$PWD
 echo work dir [$homePath] [$workPath]
 
-export PATH=$homePath/flutter/bin:$PATH
+export PUB_HOSTED_URL=https://pub.flutter-io.cn
+export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
+
+export JAVA_HOME=$homePath/jdk/
+export CLASSPATH=$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
+export PATH=$homePath/flutter/bin:$JAVA_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$homePath/run/lib:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=$homePath/run/lib/pkgconfig:$PKG_CONFIG_PATH
